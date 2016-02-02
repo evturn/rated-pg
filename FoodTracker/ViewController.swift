4fd8b66f2e25bdfd9d651b639bbed80ab4b32793
Evan Turner <evturn@gmail.com>
2016-02-02 14:37:48 -0500

    Create gesture recognizer for image view

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -35,6 +35,9 @@ class ViewController: UIViewController, UITextFieldDelegate {
   }
   
   // MARK: Actions
+  @IBAction func selectImageFromPhotoLibrary(sender: UITapGestureRecognizer) {
+  }
+  
   @IBAction func setDefaultLabelText(sender: UIButton) {
     mealNameLabel.text = "Default Text"
   }

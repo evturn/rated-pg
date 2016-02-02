fc8cc4cf7ed3d3141fff8b1b64bc4b457e373032
Evan Turner <evturn@gmail.com>
2016-02-02 15:05:13 -0500

    Create image picker controller

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
   
   // MARK: Properties
   @IBOutlet weak var nameTextField: UITextField!
@@ -36,6 +36,15 @@ class ViewController: UIViewController, UITextFieldDelegate {
   
   // MARK: Actions
   @IBAction func selectImageFromPhotoLibrary(sender: UITapGestureRecognizer) {
+    
+    // Hide the keyboard.
+    nameTextField.resignFirstResponder()
+    
+    // UIImagePickerController is a view controller that lets a user pick media from their photo library.
+    let imagePickerController = UIImagePickerController()
+    
+    // Only allow photos to be picked, not taken.
+    imagePickerController.sourceType = .PhotoLibrary
   }
   
   @IBAction func setDefaultLabelText(sender: UIButton) {

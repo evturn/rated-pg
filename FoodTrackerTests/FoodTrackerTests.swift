5a5eccf425ff456a12132e6244e40a471b163bd7
Evan Turner <evturn@gmail.com>
2016-02-04 12:57:36 -0500

    Add tests for failing during initializer

--- FoodTrackerTests/FoodTrackerTests.swift
+++ FoodTrackerTests/FoodTrackerTests.swift
@@ -19,5 +19,12 @@ class FoodTrackerTests: XCTestCase {
     //  Success case.
     let potentialItem = Meal(name: "Newest meal", photo: nil, rating: 5)
     XCTAssertNotNil(potentialItem)
+    
+    //  Failure cases.
+    let noName = Meal(name: "", photo: nil, rating: 0)
+    XCTAssertNil(noName, "Empty name is invalid")
+    
+    let badRating = Meal(name: "Really bad rating", photo: nil, rating: -1)
+    XCTAssertNotNil(badRating)
   }
 }

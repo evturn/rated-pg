561780960fc6167c3e93266e8226d5f322a8adf1
Evan Turner <evturn@gmail.com>
2016-02-05 16:53:49 -0500

    Add functions to read text field editing

--- FoodTracker/MealViewController.swift
+++ FoodTracker/MealViewController.swift
@@ -23,8 +23,11 @@ class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerCo
 
   override func viewDidLoad() {
     super.viewDidLoad()
+    
     //  Handle the text field's user input through delegate callbacks.
     nameTextField.delegate = self
+    //  Enable the Save button only if the text field has a valid Meal name.
+    checkValidMealName()
   }
   
   //  MARK: UITextFieldDelegate
@@ -36,7 +39,19 @@ class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerCo
   }
   
   func textFieldDidEndEditing(textField: UITextField) {
-
+    checkValidMealName()
+    navigationItem.title = textField.text
+  }
+  
+  func textFieldDidBeginEditing(textField: UITextField) {
+    //  Disable the Save button while editing.
+    saveButton.enabled = false
+  }
+  
+  func checkValidMealName() {
+    //  Disable the Save button if the text field is empty.
+    let text = nameTextField.text ?? ""
+    saveButton.enabled = !text.isEmpty
   }
   
   //  MARK: UIImagePickerControllerDelegate
